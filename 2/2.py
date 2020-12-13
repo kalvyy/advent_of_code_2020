d535ed6e6bba739795067b4c452dee604d9c9b8d
kalvyy <kalvyy@gmail.com>
2020-12-13 21:48:52 +0100

    day 2 second star

--- 2/2.py
+++ 2/2.py
@@ -6,18 +6,19 @@ def parse_input():
         return file.readlines()
 
 def is_password_valid(password_data):
-    counts = Counter(password_data[3])
-    range_min = password_data[0]
-    range_max = password_data[1]
+    index_1 = int(password_data[0]) - 1
+    index_2 = int(password_data[1]) - 1
     character = password_data[2]
-    return int(range_min) <= counts[character] <= int(range_max)
+    password = password_data[3]
+    return (password[index_1] == character) + (password[index_2]== character)
 
 inputs = parse_input()
 passwords_data = [re.findall(r'(\d+)-(\d+)\s(\w):\s(.*)', i) for i in inputs]
 
 counter = 0
 for password_data in passwords_data:
-    if is_password_valid(password_data[0]):
+    if is_password_valid(password_data[0]) == 1:
         counter += 1
+print(counter)
 
 #a = Counter('abcdef')
